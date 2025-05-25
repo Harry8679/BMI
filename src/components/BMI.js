efeb2af69901208666bcae772deb9f640e7c4f40
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-25 11:05:51 +0200

    BMI component 2

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const BmiCalculator = () => {
   return (
-    <div>
-      This is the BMI Calculator Component
+    <div className='Calculator'>
+      <h1>Body Mass Index Calculator</h1>
+      <div className="bmi-result-container">
+        <div className="bmi-result">
+          <div className="bmi-result-number">
+            Body Mass Index (BMI) = 23
+          </div>
+          <div className={`bmi-category`}>
+            Underweight
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

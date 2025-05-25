1b8ed3007825499a58faea2cdce4ddd55d656e41
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-25 11:00:48 +0200

    BMI component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import BmiCalculator from './components/BmiCalculator';
+import BMI from './components/BMI';
 
 const App = () => {
   return (
     <div className="App">
       <div className="row justify-content-center">
         <div className="col-lg-6 col-lg-offset-3">
-          <BmiCalculator />
+          <BMI />
         </div>
       </div>
     </div>

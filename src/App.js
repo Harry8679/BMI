e37bca6fb755817f61d4aa34efea259fbacff9ab
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-30 13:45:52 +0200

    Container added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BMI from './components/BMI';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="Appc ontainer">
       <div className="row justify-content-center">
         <div className="col-lg-6 col-lg-offset-3">
           <BMI />

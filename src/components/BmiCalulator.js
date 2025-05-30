b62ccff5af0b5bf1ea6d1a8dbe1d8c2f880198f6
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-30 14:29:39 +0200

    Bmi Calculator 3

--- src/components/BmiCalulator.js
+++ src/components/BmiCalulator.js
@@ -17,6 +17,8 @@ const BmiCalulator = () => {
             </div>
           </div>
           <FormInput type='text' name='heightCount' title={`Height (cm)`} value='' onChange={onChangeInput} />
+          <FormInput type='text' name='inchesCount' title={`(in)`} value='' onChange={onChangeInput} />
+          <FormInput type='text' name='weightCount' title={`Weight (kg)`} value='' onChange={onChangeInput} />
         </div>
         <button className='button' type='submit'>Reset</button>
       </div>

0358ae36acd0a973b8ea92c867b7f6c4b32f6cd2
Harry MacCode <emarh.harry.code@gmail.com>
2025-05-30 14:15:00 +0200

    Form Input 2

---
+++ src/components/FormInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FormInput = props => {
+  const { name, type, title, value, onChange } = props;
+
+  return (
+    <div className='input-groups'>
+      <span className="label">{title}</span>
+      <div className="range-container">
+        <input type={type} name={name} className='range-input' value={value} onChange={onChange} autoComplete='false' />
+      </div>
+    </div>
+  )
+}
+
+export default FormInput;

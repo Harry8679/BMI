0507e310d92279d17d44240e031981aae8b3d52b
Harry MacCode <emarh.harry.code@gmail.com>
2025-06-02 22:32:29 +0200

    Set BMI Value

--- src/components/BMI.js
+++ src/components/BMI.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BmiCalulator from './BmiCalulator';
 
 const BMI = () => {
+  const [bmiValue, setBmiValue] = useState(0);
   return (
     <div className='calculator'>
       <h1>Body Mass Index Calculator</h1>
       <div className="bmi-result-container">
         <div className="bmi-result">
           <div className="bmi-result-number">
-            Body Mass Index (BMI) = 23
+            Body Mass Index (BMI) = {bmiValue}
           </div>
           <div className={`bmi-category`}>
             Underweight
           </div>
         </div>
       </div>
-      <BmiCalulator />
+      <BmiCalulator setBmiValue={setBmiValue} />
     </div>
   )
 }
